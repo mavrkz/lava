388af13dd5433afcf5441caadd0009daab7d7d1d
Helmy Yahya <138000898+mavrkz@users.noreply.github.com>
2024-03-22 14:38:54 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-ca70325a-1bef-40f3-97ee-5beda2904aca/)
 
 
 echo "$curl_output" >> Update.tsx
